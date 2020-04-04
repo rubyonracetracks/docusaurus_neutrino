3c3b4955d0de2a93774cdabcd8189b021e6402fc
Jason Hsu <jhsu@midwestmortgageanalytics.com>
2020-04-03 21:25:18 -0500

    Added 01

---
+++ mod/mod-01/mod-01.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo '#########################'
+echo 'Chapter 1: Initial Commit'
+echo '#########################'
+
+echo 'npm install'
+npm install
+git init
+git add .
+git commit -m "Initial commit"
