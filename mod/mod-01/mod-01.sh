dcf2aa4a96c449b3d1604d7fe9dfbc12d1ca024a
Jason Hsu <jhsu@midwestmortgageanalytics.com>
2020-04-03 21:39:24 -0500

    Configured for Travis CI

--- mod/mod-01/mod-01.sh
+++ mod/mod-01/mod-01.sh
@@ -4,8 +4,12 @@ echo '#########################'
 echo 'Chapter 1: Initial Commit'
 echo '#########################'
 
-echo 'npm install'
-npm install
-git init
-git add .
-git commit -m "Initial commit"
+DIR_MAIN=$PWD
+
+echo '-----------------------------------'
+echo "cd $DIR_MAIN/website && npm install"
+cd $DIR_MAIN/website && npm install
+
+cd $DIR_MAIN && git init
+cd $DIR_MAIN && git add .
+cd $DIR_MAIN && git commit -m "Initial commit"
---
+++ travis-after.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+# This script is used in the Travis environment for testing the new app
+# after it is created.
+
+cd tmp1/website
+
+echo '-----------'
+echo 'npm install'
+npm install
+
+echo '--------'
+echo 'npm test'
+npm test
