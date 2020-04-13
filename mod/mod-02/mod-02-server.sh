a2d14fd539b72724a4a90e120c98fd54d2c0e331
Jason Hsu <jhsu802701@jasonhsu.com>
2020-04-13 14:32:53 -0500

    Updated server.sh

--- mod/mod-02/mod-02-server.sh
+++ mod/mod-02/mod-02-server.sh
@@ -4,6 +4,10 @@ bin/yinstall
 
 echo '-----------------'
 echo 'docker-compose up'
-echo 'View this app in your browser at http://localhost:3000/'
+echo ''
+echo 'About to run your new Docusaurus app on the local server'
+echo 'You will be able to view your app in your browser at the following URLs:'
+echo 'Docusaurus web server: http://localhost:3000'
+echo 'LiveReload web server: http://localhost:35729'
 echo ''
 docker-compose up
